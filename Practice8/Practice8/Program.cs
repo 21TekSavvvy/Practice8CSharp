64c823657246cef7204680c3a3ef0e4870af6ff8
21TekSavvvy <jareddarochajj@gmail.com>
2022-03-07 13:48:26 -0800

    Final

--- Practice8/Practice8/Program.cs
+++ Practice8/Practice8/Program.cs
@@ -93,24 +93,25 @@ class Number
      public int GetReverse()
     {
         int num = value;
-        int digit, reverse = 0;
-        Stack<int> stack = new Stack<int>();
-
+        int digit;
+       ArrayList  list = new ArrayList ();
+        string reverse="";
         while (num > 0)
         {
             digit = num % 10;
             num = num / 10;
-           stack.Push(digit);
+           list.Add(digit.ToString());
         }
-        while(stack .Count >0)
+        for (int i = 0; i < list.Count; i++)
         {
-            reverse+= stack.Pop();
+            reverse += list[i];
         }
-        return reverse;
+        
+        return int.Parse(reverse);
     }
      public string ToWords()
     {
-        int num = value;
+        int num = GetReverse();
         int digit;
         string word , answer = "";
         while (num > 0)
@@ -143,15 +144,63 @@ class Number
     }
     public bool IsArmstrong()
     {
-
+        int num = value;
+        int sum = 0, digit;
+        int count = GetCountOfDigits();
+        while (num> 0)
+        {
+            digit  =num % 10;
+            sum+= Power(digit, count, true);
+            num = num /10;
+            
+        }
+        return sum == value;
     }
      public string GetFibonacci()
     {
+        int a = 0, b = 1;
+        string fibonacci = a + ", " + b + ", ";
+        int c = a + b;
+        while (c <= value)
+        {
+            c = a + b;
+            if (c > value) 
+                break;
 
+            
+            fibonacci += c + ", ";
+
+            a = b; 
+            b = c; 
+        }
+        return fibonacci;
     }
      public bool isPalindrome()
     {
+        int num = value;
+        int reverse = GetReverse();
+        return reverse == value;
+    }
 
+    static void Main()
+    {
+        Number number = new Number();
+        number.SetValue(371); //To test we would input 371
+        System.Console.WriteLine("Value: " + number.GetValue()); //Output: 371
+        System.Console.WriteLine("IsZero: " + number.IsZero()); //Output: False
+        System.Console.WriteLine("IsPositive: " + number.IsPositive()); //Output: True
+        System.Console.WriteLine("IsNegative: " + number.IsNegative()); //Output: False
+        System.Console.WriteLine("IsOdd: " + number.IsOdd()); //Output: True
+        System.Console.WriteLine("IsEven: " + number.IsEven()); //Output: False
+        System.Console.WriteLine("IsPrime: " + number.IsPrime()); //Output: False
+        System.Console.WriteLine("GetCountOfDigits: " + number.GetCountOfDigits()); //Output: 3
+        System.Console.WriteLine("GetSumOfDigits: " + number.GetSumOfDigits()); //Output: 11
+        System.Console.WriteLine("GetReverse: " + number.GetReverse()); //Output: 173
+        System.Console.WriteLine("ToWords: " + number.ToWords()); //Output: Three Seven One
+        System.Console.WriteLine("IsArmstrong: " + number.IsArmstrong()); //Output: True
+        System.Console.WriteLine("GetFibonacci: " + number.GetFibonacci()); //Output: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233
+        System.Console.WriteLine("isPalindrome: " + number.isPalindrome()); //Output: False
+        System.Console.ReadKey();
     }
 
 }
